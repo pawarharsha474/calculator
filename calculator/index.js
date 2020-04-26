f613b5743df5c0de119213cdbf052eaeb0c82899
Harsha <43004804+pawarharsha474@users.noreply.github.com>
2020-04-26 15:05:21 +0530

    Initial commit

---
+++ calculator/index.js
@@ -0,0 +1,16 @@
+var numField1 = document.getElementById("numField1");
+var numField2 = document.getElementById("numField2");
+var resultField = document.getElementById("resultField");
+var form = document.getElementById("xispercentageofy");
+
+form.addEventListener("submit", function(event) {
+  if (!numField1.value || !numField2.value) alert("you didnt put values");
+  else {
+    var x = parseFloat(numField1.value);
+    var y = parseFloat(numField2.value);
+    var result = x / y;
+    var percent = result * 100;
+    resultField.innerText = "Your answer:" + percent + "%";
+    event.preventDefault();
+  }
+});
